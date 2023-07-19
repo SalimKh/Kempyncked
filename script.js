0fbba0e4b80f7d5d6bd594657bb95ce7da590e37
SalimKh <kamatayan164@gmail.com>
2023-07-19 15:41:54 +0200

    marcel

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var get = document.querySelector.bind(document),
   on = document.addEventListener.bind(document),
-  canvas;
+  eventCompteur = 0,
+  k;
 
 function init() {
-  canvas = get("canvas");
-  context = canvas.getContext("2d");
+  k = get("#k");
+  context = k.getContext("2d");
+
+  k.onclick = function (event) {
+    if (eventCompteur == 0) {
+      var audio = new Audio("marcel.mp3");
+      audio.play();
+    }
+  };
 }
 
 on("DOMContentLoaded", init);
