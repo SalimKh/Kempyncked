49deafd8ed955d72b64b5f2ebe4c643041e10916
SalimKh <kamatayan164@gmail.com>
2023-07-19 19:24:05 +0200

    kempynck dance

--- script.js
+++ script.js
@@ -5,15 +5,41 @@ var get = document.querySelector.bind(document),
 
 function init() {
   k = get(".k");
+  var audio = new Audio("marcel.mp3");
 
   k.onclick = function (event) {
     if (eventCompteur == 0) {
       eventCompteur++;
-      console.log(eventCompteur);
-      var audio = new Audio("marcel.mp3");
       audio.play();
+
+      k.animate(
+        [
+          // keyframes
+          { transform: "scale(0.6, 0.6)" },
+          { transform: "rotate(-0.25turn)" },
+          { transform: "scale(0.2, 0.2)" },
+          { transform: "rotate(-0.25turn)" },
+          { transform: "scale(0.6, 0.6)" },
+          { transform: "rotate(-0.25turn)" },
+          { transform: "scale(1, 1)" },
+          { transform: "rotate(-0.25turn)" },
+          { transform: "scale(1.4, 1.4)" },
+          { transform: "rotate(-0.25turn)" },
+          { transform: "scale(1.8, 1.8)" },
+          { transform: "rotate(-0.25turn)" },
+        ],
+        {
+          // timing options
+          duration: 900,
+          iterations: 10,
+        }
+      );
     }
   };
+
+  audio.addEventListener("ended", (event) => {
+    eventCompteur = 0;
+  });
 }
 
 on("DOMContentLoaded", init);
