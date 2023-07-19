87b4aab76edd99c9ec156773dd4b730129a05997
Titouan GUYADER <titouan.guyader@free.fr>
2023-07-19 20:18:37 +0200

    refactor

--- script.js
+++ script.js
@@ -1,45 +1,37 @@
-var get = document.querySelector.bind(document),
-  on = document.addEventListener.bind(document),
-  eventCompteur = 0,
-  k;
+// var get = document.querySelector.bind(document),
+//   on = document.addEventListener.bind(document),
+//   eventCompteur = 0,
+//   k;
 
-function init() {
-  k = get(".k");
-  var audio = new Audio("marcel.mp3");
+window.addEventListener("load", () => {
+  const kempynck = document.getElementById("kempynck");
 
-  k.onclick = function (event) {
-    if (eventCompteur == 0) {
-      eventCompteur++;
-      audio.play();
+  kempynck.addEventListener("click", () => {
+    const audio = document.getElementById("audio");
+    audio.volume = 0.2;
+    audio.play();
 
-      k.animate(
-        [
-          // keyframes
-          { transform: "scale(0.6, 0.6)" },
-          { transform: "rotate(-0.25turn)" },
-          { transform: "scale(0.2, 0.2)" },
-          { transform: "rotate(-0.25turn)" },
-          { transform: "scale(0.6, 0.6)" },
-          { transform: "rotate(-0.25turn)" },
-          { transform: "scale(1, 1)" },
-          { transform: "rotate(-0.25turn)" },
-          { transform: "scale(1.4, 1.4)" },
-          { transform: "rotate(-0.25turn)" },
-          { transform: "scale(1.8, 1.8)" },
-          { transform: "rotate(-0.25turn)" },
-        ],
-        {
-          // timing options
-          duration: 900,
-          iterations: 10,
-        }
-      );
-    }
-  };
-
-  audio.addEventListener("ended", (event) => {
-    eventCompteur = 0;
+    kempynck.animate(
+      [
+        // keyframes
+        { transform: "scale(0.6, 0.6)" },
+        { transform: "rotate(-0.25turn)" },
+        { transform: "scale(0.2, 0.2)" },
+        { transform: "rotate(-0.25turn)" },
+        { transform: "scale(0.6, 0.6)" },
+        { transform: "rotate(-0.25turn)" },
+        { transform: "scale(1, 1)" },
+        { transform: "rotate(-0.25turn)" },
+        { transform: "scale(1.4, 1.4)" },
+        { transform: "rotate(-0.25turn)" },
+        { transform: "scale(1.8, 1.8)" },
+        { transform: "rotate(-0.25turn)" },
+      ],
+      {
+        // timing options
+        duration: 900,
+        iterations: 10,
+      }
+    );
   });
-}
-
-on("DOMContentLoaded", init);
+});
