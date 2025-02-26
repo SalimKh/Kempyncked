cdd367805da4cd7dfff131d752920df9a45fa040
SalimKh <kamatayan164@gmail.com>
2025-02-27 00:43:28 +0100

    eee

--- js/jesaispas.js
+++ js/jesaispas.js
@@ -1,8 +1,6 @@
 // Init
 window.onload = (event) => {
-  const img1 = new Image(); // Image constructor
-  img1.src = "../ressources/camtarK.png";
-  return img1;
+  window.location.href = "../ressources/camtarK.png";
 }
 
 
